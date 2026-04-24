3da640587fb57ce874d0d3f63a02efa495a5160e
VitorCoutinho11 <vitorofficial11@gmail.com>
2026-04-23 22:59:37 -0300

    atualização 2.2

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const restify = require("restify");
 const EstoquesController = require("./controllers/estoques.controller");
 
 const server = restify.createServer({ 
-  name: "api-usuarios-restify" 
+  name: "api-estoques-restify" 
 }); 
 
 server.use(restify.plugins.queryParser()); 
@@ -10,14 +10,22 @@ server.use(restify.plugins.bodyParser());
 
 /* ROTAS DE ESTOQUES */
 server.get("/estoques", EstoquesController.listar);
-server.get("/estoques/:id", EstoquesController.buscarPorId);
+
+// rota específica primeiro
 server.get("/estoques/produto/:produtoId", EstoquesController.buscarPorProdutoId);
+server.get("/estoques/:id", EstoquesController.buscarPorId);
 
 server.post("/estoques", EstoquesController.criar);
 server.patch("/estoques/:id", EstoquesController.atualizar);
 server.del("/estoques/:id", EstoquesController.deletar);
 
+// tratamento global de erro
+server.on("restifyError", (req, res, err, callback) => {
+  console.error(err);
+  return callback();
+});
+
 const PORT = 3003; 
 server.listen(PORT, () => { 
-    console.log(`${server.name} rodando em ${server.url}`); 
-}); 
+  console.log(`${server.name} rodando em ${server.url}`); 
+});
