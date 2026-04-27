d1a3de9a3ab0e928af5760a6c99af817ee498799
VitorCoutinho11 <vitorofficial11@gmail.com>
2026-04-26 22:14:23 -0300

    atualização 3.1

--- prisma/migrations/20260414122052_create_table_usuarios/migration.sql
+++ prisma/migrations/20260414122052_create_table_usuarios/migration.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `Estoque` (
+CREATE TABLE `estoque` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `produtoId` INTEGER NOT NULL, -- Referência lógica (ID que vem da API do seu amigo)
+    `produtoId` INTEGER NOT NULL, 
     `sectionId` INTEGER NOT NULL,
     `quantidade` INTEGER NOT NULL DEFAULT 0,
-    `quantidadeMinima` INTEGER NOT NULL DEFAULT 0,
+    `quantidademinima` INTEGER NOT NULL DEFAULT 0,
     `observacao` TEXT NULL,
     `ativo` BOOLEAN NOT NULL DEFAULT TRUE,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -14,5 +14,5 @@ CREATE TABLE `Estoque` (
     UNIQUE INDEX `Estoque_produto_section_key`(`produtoId`, `sectionId`),
 
     PRIMARY KEY (`id`)
-    -- Removi as Constraints de Foreign Key para permitir bancos separados
+   
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const restify = require("restify"); 
-const EstoquesController = require("./controllers/estoques.controller");
+const EstoquesController = require("./controllers/estoques.controller"); 
+
+
+console.log("Controller carregado:", EstoquesController);
 
 const server = restify.createServer({ 
   name: "api-estoques-restify" 
@@ -8,16 +11,18 @@ const server = restify.createServer({
 server.use(restify.plugins.queryParser()); 
 server.use(restify.plugins.bodyParser()); 
 
-/* ROTAS DE ESTOQUES */
-server.get("/estoques", EstoquesController.listar);
 
-// rota específica primeiro
-server.get("/estoques/produto/:produtoId", EstoquesController.buscarPorProdutoId);
-server.get("/estoques/:id", EstoquesController.buscarPorId);
-
-server.post("/estoques", EstoquesController.criar);
-server.patch("/estoques/:id", EstoquesController.atualizar);
-server.del("/estoques/:id", EstoquesController.deletar);
+if (EstoquesController && EstoquesController.listar) {
+    server.get("/estoques", EstoquesController.listar);
+    server.get("/estoques/produto/:produtoId", EstoquesController.buscarPorProdutoId);
+    server.get("/estoques/:id", EstoquesController.buscarPorId);
+    server.post("/estoques", EstoquesController.criar);
+    server.patch("/estoques/:id", EstoquesController.atualizar);
+    server.del("/estoques/:id", EstoquesController.deletar);
+} else {
+    console.error("ERRO CRÍTICO: As funções do EstoquesController não foram encontradas!");
+    process.exit(1); 
+}
 
 // tratamento global de erro
 server.on("restifyError", (req, res, err, callback) => {
